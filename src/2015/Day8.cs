0518fb9e9d2b4ae88a293a96d5547b3ee4eb490e
Jallen <111014323+Jallenbah@users.noreply.github.com>
2025-07-01 20:54:23 +0100

    Solution for 2015 day 8 part 1

---
+++ src/2015/Day8.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode.Common;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2015;
+
+[Solution(2015, 8)]
+internal class Day8 : ISolution
+{
+    public string Solve(string input)
+    {
+        var escapeRegex = new Regex(@"\\x[A-F0-9]{2}|\\[""\\]", RegexOptions.IgnoreCase);
+
+        var strings = input.Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        var result = strings.Select(Check).Sum();
+        return $"{result}";
+
+        int Check(string s) => s.Length - escapeRegex.Replace(s, "§").Trim('"').Length;
+    }
+}
