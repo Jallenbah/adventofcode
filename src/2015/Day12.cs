97821d4809fb1b789df15c75710fafe2232ad751
Jallen <111014323+Jallenbah@users.noreply.github.com>
2025-07-07 22:22:47 +0100

    Solution for 2015 day 12 part 1

---
+++ src/2015/Day12.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Common;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2015;
+
+[Solution(2015, 12)]
+internal class Day12 : ISolution
+{
+    public string Solve(string input)
+    {
+        var part1 = _numberRegex.Matches(input).Select(m => int.Parse(m.Value)).Sum();
+
+        var part2 = DoPart2(input);
+
+        return
+            $"Part 1: {part1}\n" +
+            $"Part 2: WIP"; //{part2}";
+    }
+
+    int DoPart2(string input)
+    {
+        return 0;
+    }
+
+    private Regex _numberRegex = new Regex(@"-?\d+");
+}
